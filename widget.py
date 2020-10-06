54575fcf4efff4fb4ad96d6eb90018fddbb3d5d2
Gabriel F <gabriel_frac@outlook.com>
2020-10-05 19:53:36 -0500

    Widgets más utilizados

--- widget.py
+++ widget.py
@@ -8,7 +8,7 @@
 # ----------------------------------------------------------------------------
 
 """
-    Ventana con los widgets más comunes para un desarrollo GUi
+    Ventana con los widgets más comunes para un desarrollar GUI
 
     Python 3.8.2
     PyQt 5.15.0
@@ -17,7 +17,7 @@
 
 
 from PyQt5.QtGui import QFont, QIcon
-from PyQt5.QtCore import Qt, QTranslator, QLocale, QLibraryInfo, pyqtSlot
+from PyQt5.QtCore import Qt, QTranslator, QLocale, QLibraryInfo
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QGridLayout, QVBoxLayout, QHBoxLayout,
                              QLabel, QPushButton,  QComboBox, QSizePolicy, QCheckBox, QTreeWidget, QMessageBox)
 
@@ -37,7 +37,6 @@ class window(QMainWindow):
         font.setPointSize(9)
         self.setFont(font)
 
-
         #Objeto gráfico/Asignar a la ventana principal
         widget=widget1()
         self.setCentralWidget(widget)
@@ -56,21 +55,35 @@ class widget1(QWidget):
     #Variables-widget
     self.list_periodo=self.periodo()
         
-    #Objeto: Qtable- numero de columnas/registros
+    #-------Objeto: Qtable-------------------
     self.table_user=QTreeWidget()
-    
     #Campos de la tabla
+    self.table_user.setHeaderLabels(("Usuario", "Pass", "Nombre", "Apellido", "Fecha de Nacimiento"))
     
+    #Formato
+    self.model=self.table_user.model()
 
-    #Combobox
-    combobox1=QComboBox()
-    combobox1.addItem("Hola")
-    combobox1.addItem("Hola2")
-    combobox2=QComboBox()
-    combobox3=QComboBox()
-    combobox4=QComboBox()
+    for indice, ancho in enumerate((110, 150, 150, 160), start=0):
+            self.model.setHeaderData(indice, Qt.Horizontal, Qt.AlignCenter, Qt.TextAlignmentRole)
+            self.table_user.setColumnWidth(indice, ancho)
+
+    self.table_user.setAlternatingRowColors(True)
     
-    #Layout vertical para combobox
+    #-------------Combobox------------------
+    combobox1=QComboBox()
+    combobox1.addItem("Periodo 1")
+    combobox1.addItem("Periodo 2")
+    combobox2=QComboBox()
+    combobox2.addItem("Categoría 1")
+    combobox2.addItem("Categoría 2")
+    combobox3=QComboBox()
+    combobox3.addItem("Sub-categoría 1")
+    combobox3.addItem("Sub-categoría 1")
+    combobox4=QComboBox()
+    combobox4.addItem("Marca 1")
+    combobox4.addItem("Marca 2")
+    
+    #----------Objeto Layout vertical para combobox--------
     vertical_layout=QVBoxLayout()
     vertical_layout.addWidget(QLabel("Seleccionar periodo"))
     vertical_layout.addWidget(combobox1)
@@ -82,37 +95,41 @@ class widget1(QWidget):
     vertical_layout.addWidget(combobox4)
     vertical_layout.addStretch()#expandir
 
+    #----------Objeto Layout vertical para QPushButton--------
     horizontal_layout=QHBoxLayout()
     horizontal_layout.addWidget(QPushButton("Descargar"))
     horizontal_layout.addWidget(QPushButton("Imprimir"))
     horizontal_layout.addWidget(QPushButton("Limpiar"))
     horizontal_layout.addStretch()
 
-    #Layout
+    #---------Objeto - Layout-----------------
     gridLayout1=QGridLayout()
     #Agregar al gridlayout1
     gridLayout1.addLayout(vertical_layout,   0, 0, 2, 1)#layout, (row, col)
     gridLayout1.addLayout(horizontal_layout, 0, 1, 1, 1)
     gridLayout1.addWidget(self.table_user,   1, 1, 1, 1)#widget 
 
-
-    #Asignar al layout clase: widget
+    #Asignar al layout clase widget
     self.setLayout(gridLayout1)
 
     #--------------Eventos combobox----------------------------------------
     combobox1.currentIndexChanged.connect(self.message)
-
-
+    combobox2.currentIndexChanged.connect(self.message)
+    combobox3.currentIndexChanged.connect(self.message)
+    combobox4.currentIndexChanged.connect(self.message)
     
   def message(self):
 
-    print("Hola")
+        msg=QMessageBox(self)
+        msg.setWindowTitle("Mensaje")
+        msg.setText("Es un mensaje")
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec_()
 
   def periodo(self):
 
       return 0
   
-
 
 if __name__ == "__main__":
     
